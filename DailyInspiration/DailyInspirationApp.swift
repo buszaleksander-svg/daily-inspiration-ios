efb0c3d9f288e3d98e390d1e1913bc658e5c4028
aleksander busz fabritius <buszaleksander@gmail.com>
2025-12-27 15:16:08 +0100

    Update app entry and cleanup StartView

--- DailyInspiration/DailyInspirationApp.swift
+++ DailyInspiration/DailyInspirationApp.swift
@@ -1,17 +1,10 @@
-//
-//  DailyInspirationApp.swift
-//  DailyInspiration
-//
-//  Created by Aleksander Busz Fabritius on 2025-12-27.
-//
-
 import SwiftUI
 
 @main
 struct DailyInspirationApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            StartView()
         }
     }
 }
