ae0d735a56efc1415d336767e0c7acbba7ad5ffc
aleksander busz fabritius <buszaleksander@gmail.com>
2025-12-27 12:32:44 +0100

    Initial Commit

---
+++ DailyInspiration/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyInspiration
+//
+//  Created by Aleksander Busz Fabritius on 2025-12-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyInspiration/DailyInspirationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyInspirationApp.swift
+//  DailyInspiration
+//
+//  Created by Aleksander Busz Fabritius on 2025-12-27.
+//
+
+import SwiftUI
+
+@main
+struct DailyInspirationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
