0558d6db30b2799e19cf94ef36eca25db3156ec9
Muhammad Jawad <jawadbqa@gmail.com>
2026-01-04 15:58:47 +0100

    Add @State favoriteQuotes array with sample button in FavoritesView

--- DailyInspiration/Views/FavoritesView.swift
+++ DailyInspiration/Views/FavoritesView.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct FavoritesView: View {
+    
+    @State var favoriteQuotes: [String] = []
+    
+    var body: some View {
+        VStack {
+            Text("Favorite Quote")
+                .font(.largeTitle)
+                .padding()
+            
+            List(favoriteQuotes, id: \.self) { quote in
+                Text(quote)
+            }
+            Button("Favorite Quote") {
+                // to add a new quote in the list
+                favoriteQuotes.append("You are reading the favorite quote!")
+            }
+            .padding()
+        }
+    }
+}
+
+
+#Preview {
+    FavoritesView()
+}
